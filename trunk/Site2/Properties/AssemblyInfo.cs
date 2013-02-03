b56bb42278a29aba1bee757025d90e8ac74c78df
Giovanni Bassi <giggio@giggio.net>
2013-02-03 21:57:52 -0200

    moving files forward to a new project

--- trunk/Site2/Properties/AssemblyInfo.cs
+++ trunk/Site2/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("site")]
+[assembly: AssemblyTitle("Site2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("site")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Site2")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0f2e80c5-989d-419a-aa55-5f851f60aa5f")]
+[assembly: Guid("7a6d888c-b955-4243-8e4d-80b2559ece24")]
 
 // Version information for an assembly consists of the following four values:
 //
